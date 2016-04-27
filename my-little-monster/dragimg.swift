e0cc4003e45667f0f05d6dd66ca0b56e01ecbcf1
BilldoM <williammelvin26@yahoo.com>
2016-04-26 22:14:45 -0400

    part 3 completed

---
+++ my-little-monster/dragimg.swift
@@ -0,0 +1,9 @@
+//
+//  dragimg.swift
+//  my-little-monster
+//
+//  Created by William Melvin on 4/13/16.
+//  Copyright © 2016 William Melvin. All rights reserved.
+//
+
+import Foundation
---
+++ my-little-monster/monsterimg.swift
@@ -0,0 +1,9 @@
+//
+//  monsterimg.swift
+//  my-little-monster
+//
+//  Created by William Melvin on 4/26/16.
+//  Copyright © 2016 William Melvin. All rights reserved.
+//
+
+import Foundation
