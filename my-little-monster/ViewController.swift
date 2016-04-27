a17274d3b7b413481332012227556d3a69431490
BilldoM <williammelvin26@yahoo.com>
2016-04-26 22:15:46 -0400

    made it through part 3

--- my-little-monster/ViewController.swift
+++ my-little-monster/ViewController.swift
@@ -9,17 +9,81 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImg: MonsterImg!
+    @IBOutlet weak var foodImage: DragImg!
+    @IBOutlet weak var heartImage: DragImg!
+    @IBOutlet weak var penaltyImg: UIImageView!
+    @IBOutlet weak var penaltyImg2: UIImageView!
+    @IBOutlet weak var penaltyImg3: UIImageView!
 
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    
+    var penalties = 0
+    var timer: NSTimer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        foodImage.dropTarget = monsterImg
+        heartImage.dropTarget = monsterImg
+        
+        penaltyImg.alpha = DIM_ALPHA
+        penaltyImg2.alpha = DIM_ALPHA
+        penaltyImg3.alpha = DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
+    
+        startTimer()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    func itemDroppedOnCharacter(notif: AnyObject) {
     }
-
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        
+        penalties = penalties + 1
+        
+        if penalties == 1 {
+            penaltyImg.alpha = OPAQUE
+            penaltyImg2.alpha = DIM_ALPHA
+        } else if penalties == 2 {
+            penaltyImg2.alpha = OPAQUE
+            penaltyImg3.alpha = DIM_ALPHA
+        } else if penalties >= 3 {
+            penaltyImg3.alpha = OPAQUE
+        } else {
+            penaltyImg.alpha = DIM_ALPHA
+            penaltyImg2.alpha = DIM_ALPHA
+            penaltyImg3.alpha = DIM_ALPHA
+        }
+        
+        if penalties >= MAX_PENALTIES {
+            gameOver()
+        }
+        
+        
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImg.playDeathAnimation()
+    }
 
 }
 
--- my-little-monster/monsterimg.swift
+++ my-little-monster/monsterimg.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import UIKit
+
+class MonsterImg: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        playIdleAnimation()
+    }
+    
+    func playIdleAnimation() {
+        
+        self.image = UIImage(named: "idle1.png")
+        
+        self.animationImages = nil
+        
+        
+        var imageArray = [UIImage]()
+        for x in 1..<4 {
+            let img = UIImage(named: "idle\(x).png")
+            imageArray.append(img!)
+        }
+        
+        self.animationImages = imageArray
+        self.animationDuration = 0.85
+        self.animationRepeatCount = 0
+        self.startAnimating()
+        
+    }
+    
+    func playDeathAnimation() {
+        
+        self.image = UIImage(named: "dead5.png")
+        
+        self.animationImages = nil
+        
+        var imageArray = [UIImage]()
+        for x in 1..<5 {
+            let img = UIImage(named: "dead\(x).png")
+            imageArray.append(img!)
+        }
+        
+        self.animationImages = imageArray
+        self.animationDuration = 0.85
+        self.animationRepeatCount = 1
+        self.startAnimating()
+        
+    }
+
+    
+}
